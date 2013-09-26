f4628e8b28226d649d80dcdfd36b512905236fbd
Kevin Oishi <kevin.oishi@gmail.com>
2013-09-26 13:03:00 -0700

    done\!

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "StaticPages" do
     end
   	it "should have the right title" do
   		visit '/static_pages/home'
-  		expect(page).to have_title("#{base_title} | Home")
+  		expect(page).to have_title("#{base_title}")
   	end
   end
 
