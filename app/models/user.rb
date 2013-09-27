64bf8818cab8d7915e323d5ea69cf54fec56101a
Kevin Oishi <kevin.oishi@gmail.com>
2013-09-27 14:04:21 -0700

    basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+	before_save {self.email = email.downcase}
+
+	validates :name,  
+		presence: true, 
+		length: {maximum: 50}
+
+	# not RFC 5322
+	VALID_EMAIL_REGEX = /\A[\w\+\-\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, 
+ 		presence: true, 
+ 		length: {maximum: 50}, 
+ 		format: {with: VALID_EMAIL_REGEX},
+ 		uniqueness: {case_sensitive: false}
+
+ 	validates :password,
+ 		length: {minimum: 6}
+
+	has_secure_password
+	# attr_accessor :password, :password_confirmation, :authenticate
+end
